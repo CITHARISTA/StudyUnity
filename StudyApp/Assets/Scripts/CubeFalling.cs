954915095b2c94efde6b8466a9690680cef9ab6a
CITHARISTA <sashakosar@mail.ru>
2022-05-17 22:46:35 +0300

    added script for cube falling

--- StudyApp/Assets/Scripts/CubeFalling.cs
+++ StudyApp/Assets/Scripts/CubeFalling.cs
@@ -1,12 +1,14 @@
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
 
 public class CubeFalling : MonoBehaviour
 {
-    public float speed = 5f;
+    public float speed = 1f;
     public float distance = 5f;
     public Vector3 startPosition = new Vector3(0, 20, 0);
     public Vector3 moveDirection;
+    public float timeDeleySec = 0;
 
     private readonly Queue<GameObject> _queueOfObjects = new Queue<GameObject>();
     private GameObject _lastCreatedCube;
@@ -14,7 +16,7 @@ public class CubeFalling : MonoBehaviour
 
     private void Start()
     {
-        _queueOfObjects.Enqueue(CreateCube());
+        StartCoroutine(Waiter());
     }
 
     private void Update()
@@ -22,33 +24,30 @@ public class CubeFalling : MonoBehaviour
         foreach (var cube in _queueOfObjects)
             cube.transform.Translate(moveDirection * speed * Time.deltaTime);
 
-        if (_lastCreatedCube != null && Vector3.Distance(_lastCreatedCube.transform.position, startPosition) > distance)
+        var heading = startPosition - _lastCreatedCube.transform.position;
+        heading.y = 0;
+        if (heading.magnitude > distance)
             _queueOfObjects.Enqueue(CreateCube());
+
+        if (_queueOfObjects.Peek().transform.position.y < -1)
+            Destroy(_queueOfObjects.Dequeue());
     }
 
     private GameObject CreateCube()
     {
-        if (_lastCreatedCube != null)
-            return Instantiate(_lastCreatedCube)
-                .SetStartPosition(startPosition);
-
-        var cube = GameObject
-            .CreatePrimitive(PrimitiveType.Cube)
-            .SetStartPosition(startPosition);
-
+        var cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        cube.transform.position = startPosition;
         cube.AddComponent<Rigidbody>().isKinematic = false;
 
         _lastCreatedCube = cube;
 
         return cube;
     }
-}
 
-public static class CubeExtention
-{
-    public static GameObject SetStartPosition(this GameObject cube, Vector3 startPosition)
+    private IEnumerator<WaitForSeconds> Waiter()
     {
-        cube.transform.position = startPosition;
-        return cube;
+        yield return new WaitForSeconds(timeDeleySec);
+        _queueOfObjects.Enqueue(CreateCube());
     }
 }
+
